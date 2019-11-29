33c7691a78097b8a9051daed15286c1813f82437
netbot <antidoteprog@gmail.com>
2019-11-29 10:20:56 +0300

    adding a backgroung to demo..

---
+++ js_files/arrays.js
@@ -0,0 +1,13 @@
+var person = ["bmw", "x6", "holland"];//an array
+
+var person = {
+    first_name: "john",
+    second_name: "doe",
+    DOB: "01-09-2000"
+};
+
+
+function myfunction(a1, a2){
+    return a1*a2;
+}
+console.log(myfunction(4, 3))
